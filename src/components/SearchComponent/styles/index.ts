ccd4256e8f2b11be6cec945486fca21e0258c398
Leonardo Lima <leonardo.b.lima1@gmail.com>
2022-02-08 17:43:03 -0300

    search fix

--- src/components/SearchComponent/styles/index.ts
+++ src/components/SearchComponent/styles/index.ts
@@ -2,7 +2,21 @@ import styled from "styled-components";
 
 
 export const SearchFormStyled = styled.form`
+  display: flex;
+  justify-content: center;
+  background-color: white;
+  padding: 30px;
+  border-radius: 5px;
+  border: 1px solid var(--gray-200);
 
 
+  label{
+    width: 40%;
+    margin-right: 10px;
+  }
+
+  button{
+    max-width: 200px;
+  }
 
 `;
